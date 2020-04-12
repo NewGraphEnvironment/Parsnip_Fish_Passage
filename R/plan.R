72d1839fb25c3e31d705e4f43eabeb7e84af8442
NewGraphEnvironment <al@newgraphenvironment.com>
2020-04-11 17:37:29 -0700

    filter empty rows in loc_data and test plan2 addition

--- R/plan.R
+++ R/plan.R
@@ -1,5 +1,5 @@
 # The workflow plan data frame outlines what you are going to do.
-plan <- drake_plan(
+plan1 <- drake_plan(
   data_raw =file_in("./data/parsnip_habitat_assessments.xls") %>% 
     excel_sheets() %>% 
     set_names() %>% 
@@ -11,18 +11,25 @@ plan <- drake_plan(
   site_data = data_raw %>% 
     purrr::pluck("step_4_stream_site_data"), 
   loc_data = data_raw %>% 
-    purrr::pluck("step_1_ref_and_loc_info"), 
+    purrr::pluck("step_1_ref_and_loc_info") %>% 
+    dplyr::filter(!is.na(site_number)), 
   fish_data = data_raw %>% 
     purrr::pluck("step_2_fish_coll_data"),
   tracks = st_read(file_in("./data/field_cleaned.gpx"), layer = "tracks") %>% 
     separate(name, into = c('site', 'direction', 'track_num'), remove = F),
   track_points = read_sf(file_in("./data/field_cleaned.gpx"), layer = "track_points") %>% 
     separate(name, into = c('site', 'direction', 'track_num'), remove = F),
+  table = make_table(loc_dat = loc_data, site_dat = site_data),
   report = rmarkdown::render(
     knitr_in("pars_125000.Rmd"),
     output_file = file_out("./docs/pars_125000.html"),
     quiet = TRUE
   )
+)
+
+plan2 <- drake_plan(
+  track_points2 = read_sf(file_in("./data/field_cleaned.gpx"), layer = "track_points") %>% ##this is just a test
+  separate(name, into = c('site', 'direction', 'track_num'), remove = F)
 )
 
 
